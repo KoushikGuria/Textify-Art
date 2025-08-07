e0f75bf12353be72791305def9a40dd7b167916b
Koushik Guria <koushikguria998@gmail.com>
2025-08-07 18:57:41 +0530

    Login & Sign up features added

--- client/src/context/AppContextProvider.jsx
+++ client/src/context/AppContextProvider.jsx
@@ -3,8 +3,11 @@ import { AppContext } from "./AppContext";
 
 const AppContextProvider = ({ children }) => {
   const [user, setUser] = useState(null);
+  const [showLogin, setShowLogin] = useState(false);
 
-  const value = { user, setUser };
+  const value = { 
+    user, setUser, showLogin, setShowLogin 
+  };
 
   return (
     <AppContext.Provider value={value}>
