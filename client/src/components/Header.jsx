5e74d413f3e7fcf6f6e041b30fb8c19007b52c05
Koushik Guria <koushikguria998@gmail.com>
2025-08-07 10:53:55 +0530

    Result page added

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -12,7 +12,7 @@ const Header = () => {
         
         <p className='text-center max-w-xl mx-auto mt-5'>Unleash your creativity with AI. Turn your imagination into visual art in seconds - just type, and watch the magic happen.</p>
 
-        <button className='sm:text-lg text-white bg-black w-auto mt-8 px-12 py-2.5 flex items-center gap-2 rounded-full cursor-pointer'>
+        <button className='sm:text-lg text-white bg-black w-auto mt-8 px-12 py-2.5 flex items-center gap-2 rounded-full cursor-pointer hover:scale-105 transition-all duration-500'>
           Generate Images 
           <img className='h-6' src={assets.star_group} alt="" />
         </button>
